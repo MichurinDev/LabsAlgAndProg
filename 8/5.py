0a1a42f9784588c4102770e97f7d0148cfece598
MichurinDev <andreymithurin00@gmail.com>
2024-12-09 10:28:25 +0700

    Переписал встроенные min/max на свои

--- 8/5.py
+++ 8/5.py
@@ -4,19 +4,36 @@
 
 import numpy as np
 
+def sort(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+def minimum(*args):
+    arr = list(args)
+    return sort(arr)[0]
+
+
+def maximum(*args):
+    arr = list(args)
+    return sort(arr)[-1]
+    
+
 def extract_subarray(array: np.ndarray, center_row: int, center_col: int, size):
     row_count, col_count = size
 
     if center_row == 0 or center_row == row_count - 1 or center_col == 0 or center_col == col_count - 1:
         return [[array[center_row, center_col]]]
 
-    half_size = min(center_col + 1, center_row + 1, row_count - center_row, col_count - center_col) - 1
-    print(half_size)
+    half_size = minimum(center_col + 1, center_row + 1, row_count - center_row, col_count - center_col) - 1
 
-    start_row = max(center_row - half_size, 0)
-    end_row = min(center_row + half_size + 1, array.shape[0])
-    start_col = max(center_col - half_size, 0)
-    end_col = min(center_col + half_size + 1, array.shape[1])
+    start_row = maximum(center_row - half_size, 0)
+    end_row = minimum(center_row + half_size + 1, array.shape[0])
+    start_col = maximum(center_col - half_size, 0)
+    end_col = minimum(center_col + half_size + 1, array.shape[1])
 
     subarray = array[start_row:end_row, start_col:end_col]
     
